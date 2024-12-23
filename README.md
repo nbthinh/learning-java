b778b2050a709dc04e3798187420e65fb7110203
nbthinh <nguyenbuuthinh036@gmail.com>
2024-12-23 14:50:17 +0700

    chú thích

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 - Download jdk 21
+
+# Lý thuyết
+
+- https://kungfutech.edu.vn/bai-viet/spring-boot/restful-api-rest-controller-request-body-path-variable
+- @RestController là một ký hiệu tiện lợi hơn trong việc kết hợp @Controller và @ResponseBody
+- Chúng ta có thể ký hiệu các controllers cổ điển với ký hiệu @Controller. Nó là một đặc tả đơn giản của class @Component, nó cho phép chúng ta tự động phát hiện các class được thực hiện thông qua việc quét các class.
+- Hiển nhiên chúng ta sử dụng @Controller kết hợp với @RequestMapping cho các phương thức xử lý (handling) request.
+- @RestController là một phiên bản đặc biệt của controller. Nó bao gồm @Controller và @ResponseBody. Mỗi phương thức xử lý request của class controller sẽ tự động tuần tự hoá đối tượng trả về vào HttpResponse.
+
+
+- @RestController là một chú thích quan trọng trong Spring Boot. Khác với @Controller, nó không trả về một template mà thay vào đó trả về dữ liệu dưới dạng JSON.
+- Khi bạn xây dựng các API, thông tin từ phía client thường được gửi trong phần Body của request dưới dạng JSON.
+- Spring Boot sẽ tự động chuyển đổi chuỗi JSON trong request thành một đối tượng Java. Bạn chỉ cần sử dụng chú thích @RequestBody để cho Spring Boot biết bạn muốn chuyển đổi JSON thành đối tượng Java.
--- src/main/java/com/thinkconstructive/rest_demo/controller/CloudVendorApiService.java
+++ src/main/java/com/thinkconstructive/rest_demo/controller/CloudVendorApiService.java
@@ -3,18 +3,26 @@ package com.thinkconstructive.rest_demo.controller;
 import com.thinkconstructive.rest_demo.model.CloudVendor;
 import org.springframework.web.bind.annotation.*;
 
+
+// @RestController là một chú thích quan trọng trong Spring Boot. Khác với @Controller, nó không trả về một template mà thay vào đó trả về dữ liệu dưới dạng JSON.
+// Các đối tượng được trả về sẽ tự động được Spring Boot chuyển đổi thành JSON. Bạn có thể trả về nhiều loại đối tượng như List, Map, và nó sẽ được chuyển đổi một cách tự động. Spring Boot sử dụng Jackson converter mặc định để thực hiện việc này. Nếu bạn muốn tùy chỉnh kiểu dữ liệu trả về, bạn có thể sử dụng đối tượng ResponseEntity.
+
 @RestController
 @RequestMapping(value="/cloudvendor")
 public class CloudVendorApiService {
+    // GetMapping => Phương thức get
+    // GetMapping("{vendorId}") tức là {vendorId} sẽ được append vào url gốc => /cloudvendor/{vendorId}
+    // @PathVariable (name = "vendorId") sẽ cho ta lấy giá trị {vendorId} trong url /cloudvendor/{vendorId} và parse nó sang kiểu string
     CloudVendor cloudVendor;
     @GetMapping("{vendorId}")
-    public CloudVendor getCloudVendorDetails(String vendorId) {
+    public CloudVendor getCloudVendorDetails(@PathVariable(name = "vendorId") String vendorId) {
 //        return new CloudVendor("C1", "Vendor 1", "Address One", "xxxxxx");
         System.out.println("Phương thức 1 chạy");
-        System.out.println(cloudVendor);
+        System.out.println(vendorId);
         return cloudVendor;
     }
 
+    // RequestBody sẽ tự động chuyển đổi kiểu dữ liệu gửi lên (có thể dạng JSON) sang kiểu dữ liệu object của JAVA, khi đó các properties của json phải cùng tên với các project properties trong object của java
     @PostMapping
     public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
         System.out.println("cloudVendor");
